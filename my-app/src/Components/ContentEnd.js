8ecff3747935409508aba51d0682a4995ac2c995
Stacy <stacyareas@gmail.com>
2018-11-13 17:32:36 -0800

    refactor css

--- my-app/src/Components/ContentEnd.js
+++ my-app/src/Components/ContentEnd.js
@@ -4,32 +4,32 @@ const ContentEnd = () => (
 	<div>
 		<div className="row">
 			<div className="col-md-12">
-				<h1 className="text-center">Loperm Ipsum</h1>
+				<h1 className="text-center content-end-header"><span className="header-1">Loperm</span> <span className="header-2">Ipsum</span></h1>
 			</div>
 		</div>
 
 		<div className="row">
 			<div className="col-md-6">
-				<h3>Loperm ipsum dolor sit amet</h3>
-				<h4>Loperm ipsum dolor sit amet lorem</h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean. Purus viverra accumsan in nisl nisi scelerisque eu. Duis ut diam quam nulla porttitor. Suspendisse sed nisi lacus sed viverra.</p>
+				<h3 className="header-3">Loperm ipsum dolor sit amet</h3>
+				<h4 className="header-4">Loperm ipsum dolor sit amet lorem</h4>
+				<p className="paragraph-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean. Purus viverra accumsan in nisl nisi scelerisque eu. Duis ut diam quam nulla porttitor. Suspendisse sed nisi lacus sed viverra.</p>
 			</div>
 
 			<div className="col-md-6">
-				<img src="../images/img1.jpg" alt=""/>
+				<img className="content-end-imgs" src="../images/img1.jpg" alt=""/>
 			</div>
 		</div>
 
 		<div className="row">
 
 			<div className="col-md-6">
-				<img src="../images/img2.jpg" alt=""/>
+				<img className="content-end-imgs" src="../images/img2.jpg" alt=""/>
 			</div>
 
 			<div className="col-md-6">
-				<h3>Loperm ipsum dolor sit amet</h3>
-				<h4>Loperm ipsum dolor sit amet lorem</h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean. Purus viverra accumsan in nisl nisi scelerisque eu. Duis ut diam quam nulla porttitor. Suspendisse sed nisi lacus sed viverra.</p>
+				<h3 className="header-3">Loperm ipsum dolor sit amet</h3>
+				<h4 className="header-4">Loperm ipsum dolor sit amet lorem</h4>
+				<p className="paragraph-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean. Purus viverra accumsan in nisl nisi scelerisque eu. Duis ut diam quam nulla porttitor. Suspendisse sed nisi lacus sed viverra.</p>
 			</div>
 		</div>
 	</div>
--- my-app/src/Components/ContentMiddle.js
+++ my-app/src/Components/ContentMiddle.js
@@ -4,42 +4,42 @@ const ContentMiddle = () => (
 	<div className="container">
 		<div className="row">
 			<div className="col-md-12">
-				<h1 className="text-center">Loperm Ipsum</h1>
+				<h1 className="text-center main-header"><span className="header-1">Loperm</span> <span className="header-2">Ipsum</span></h1>
 			</div>
 
 		</div>
 
 		<div className="row">
 			<div className="col-md-6">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean. Purus viverra accumsan in nisl nisi scelerisque eu. Duis ut diam quam nulla porttitor. Suspendisse sed nisi lacus sed viverra.
+				<span className="col-1-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean. Purus viverra accumsan in nisl nisi scelerisque eu. Duis ut diam quam nulla porttitor. Suspendisse sed nisi lacus sed viverra.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean. Purus viverra accumsan in nisl nisi scelerisque eu. Duis ut diam quam nulla porttitor. Suspendisse sed nisi lacus sed viverra.</span>
 			</div>
 
 			<div className="col-md-6">
 				<div className="text-center">
-				<button type="button" className="btn btn-primary">Primary</button>
+				<button type="button" className="paragraph-btn">Button</button>
 				</div>
 			</div>
 		</div>
 
-		<div className="row">
+		<div className="row middle-col-content">
 			<div className="col-md-4">
 				<div className="text-center">
-					<h1>240%</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					<div className="percentage">240%</div>
+					<p className="percentage-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 				</div>
 			</div>
 
 			<div className="col-md-4">
 				<div className="text-center">
-					<h1>105%</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					<div className="percentage">105%</div>
+					<p className="percentage-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 				</div>
 			</div>
 
 			<div className="col-md-4">
 				<div className="text-center">
-					<h1>159%</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					<div className="percentage">159%</div>
+					<p className="percentage-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 				</div>
 			</div>
 		</div>
